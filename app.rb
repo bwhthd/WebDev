4265197c4f6ddc0c96dd0d4836a9c79af990c66a
Ben Whitehead <bdwhthd@gmail.com>
2014-10-16 12:49:32 -0700

    Postgres in production

--- app.rb
+++ app.rb
@@ -10,11 +10,15 @@ require 'bundler/setup'
 Bundler.require
 require './models/TodoItem.rb'
 
-ActiveRecord::Base.establish_connection(
-  :adapter  => 'sqlite3',
-  :database => 'db/development.db',
-  :encoding => 'utf8'
-)
+if ENV['DATABASE_URL']
+  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+else
+  ActiveRecord::Base.establish_connection(
+    :adapter  => 'sqlite3',
+    :database => 'db/development.db',
+    :encoding => 'utf8'
+  )
+end
 
 get '/' do
 	@allItems = TodoItem.all
