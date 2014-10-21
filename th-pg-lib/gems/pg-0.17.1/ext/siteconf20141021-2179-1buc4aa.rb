6427070b93b1b830bb80d7453eb66f3db5ed8ccc
Ben Whitehead <bdwhthd@gmail.com>
2014-10-21 14:26:59 -0700

    date sorting

---
+++ th-pg-lib/gems/pg-0.17.1/ext/siteconf20141021-2179-1buc4aa.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20141021-2179-umho8a"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
